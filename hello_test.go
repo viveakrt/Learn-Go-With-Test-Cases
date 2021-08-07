d515672198a72aba4573a8eca565aaf2e2884d7c
Vivek <vivek.y@purplle.com>
2021-08-08 00:21:36 +0530

    Added spanish, french

--- hello_test.go
+++ hello_test.go
@@ -10,15 +10,26 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Vivek")
+		got := Hello("Vivek", "")
 		want := "Hello, Vivek"
 
 		assertCorrectMessage(t, got, want)
 
 	})
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Yogeta", "Spanish")
+		want := "Hola, Yogeta"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Yogeta", "French")
+		want := "Bonjour, Yogeta"
 		assertCorrectMessage(t, got, want)
 	})
 
