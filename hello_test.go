355b2de7d88972742b34ac2ed7d55ecfb86941a4
Vivek <vivek.y@purplle.com>
2021-08-07 20:44:43 +0530

    amend

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Vivek")
-	want := "Hello, Vivek"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Vivek")
+		want := "Hello, Vivek"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
