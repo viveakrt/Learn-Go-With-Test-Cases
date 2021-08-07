8290ac4d66d1c4eda94e33e269bc97e16a368198
Vivek <vivek.y@purplle.com>
2021-08-07 20:23:46 +0530

    Added constant

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Vivek"))
 }
 
-func Hello() string {
-	return "Hello, World"
+func Hello(name string) string {
+	return englishHelloPrefix + name
 }
--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World"
+	got := Hello("Vivek")
+	want := "Hello, Vivek"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
