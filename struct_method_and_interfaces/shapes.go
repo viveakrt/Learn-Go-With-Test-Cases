a9005c8de9ac25f70c0fae603772529c27367ea3
Vivek <vivek.y@purplle.com>
2021-08-09 11:20:14 +0530

    Added method

--- struct_method_and_interfaces/shapes.go
+++ struct_method_and_interfaces/shapes.go
@@ -4,6 +4,9 @@ type Rectangle struct {
 	width  float64
 	height float64
 }
+type Circle struct {
+	Radius float64
+}
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
